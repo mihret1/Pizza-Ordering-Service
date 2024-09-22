39cc947a07599f9e60b6602367516a29113e8724
mihret1 <90724655+mihret1@users.noreply.github.com>
2024-09-22 20:42:03 +0300

    done the first part of home page

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -15,36 +15,36 @@ const Navbar=()=>{
     const isMediumSize=useMediaQuery( theme.breakpoints.down('md'))
     const [navv, setNavv]=useState()
     return(
-        <Stack direction='column' position='fixed' width='100%' className='navu'> 
+        <Stack direction='column'  width='100%' bgcolor='#FFFBF8' className='navu' > 
         <Stack direction='row'  alignItems='center' justifyContent='space-between' sx={{  paddingX:2 }} >
             <Link to='/' style={{ textDecoration:'none' }}>
               <Stack direction='row' alignItems='center' spacing={2}>
-                 <Box > <img src={logo2} style={{ height:isMediumSize?65 :92,   }}/> </Box>
-                 <Typography  sx={{ color:'brown' , fontWeight:'bolder', fontSize: isMediumSize?28: 33}}>Pizza</Typography>
+                 <Box > <img src={logo2} style={{ height:isMediumSize?53 :78,   }}/> </Box>
+                 <Typography  sx={{ color:'brown' , fontWeight:'bolder', fontSize: isMediumSize?25: 30}}>Pizza</Typography>
               </Stack>
             </Link>
             <Stack direction='row'   spacing={{xs:2, sm:3, md:5,lg:10 }} alignItems='center'>
-             <Link to='/' style={{ textDecoration:'none', color:'black', }}> <Typography  sx={{fontSize:25, '&:hover':{color:'brown'},[theme.breakpoints.down('sm')]:{fontSize:19} }}>Home</Typography></Link> 
-             <Link to='/' style={{ textDecoration:'none' ,color:'black'}}>   <Typography   sx={{fontSize:25, '&:hover':{color:'brown'},[theme.breakpoints.down('sm')]:{fontSize:19} }}>Orders</Typography></Link> 
-               {!isMediumSize &&   <Link to='/' style={{ textDecoration:'none',color:'black' }}>   <Typography  sx={{'&:hover':{color:'brown'},  fontSize:25}}>Who We are</Typography></Link> }
+             <Link to='/' style={{ textDecoration:'none', color:'black', }}> <Typography  sx={{fontSize:25, '&:hover':{color:'#FF890E'},[theme.breakpoints.down('sm')]:{fontSize:19} }}>Home</Typography></Link> 
+             <Link to='/' style={{ textDecoration:'none' ,color:'black'}}>   <Typography   sx={{fontSize:25, '&:hover':{color:'#FF890E'},[theme.breakpoints.down('sm')]:{fontSize:19} }}>Orders</Typography></Link> 
+               {!isMediumSize &&   <Link to='/' style={{ textDecoration:'none',color:'black' }}>   <Typography  sx={{'&:hover':{color:'#FF890E'},  fontSize:25}}>Who We are</Typography></Link> }
             </Stack>
             {isMediumSize && <IconButton onClick={()=>setNavv((e)=>!e)}>
                 <MenuIcon fontSize='large' />
             </IconButton>}
            <Hidden mdDown>
              <ButtonBase   component={Link} 
-              to="/target-path"  sx={{'&:hover':{backgroundColor:'brown'}, backgroundColor:'#ff6900', paddingX:6, height:50,color:'white', fontSize:22, fontWeight:'bold', borderRadius:1 }}>
+              to="/"  sx={{'&:hover':{backgroundColor:'brown'}, backgroundColor:'#FF890E', paddingX:6, height:50,color:'white', fontSize:22, fontWeight:'bold', borderRadius:1 }}>
                 Register
             </ButtonBase>
             </Hidden> 
         </Stack>
        { navv && <Hidden mdUp sx={{ }}  >
         <Stack spacing={2} sx={{  backgroundColor:'' }}> 
-        <Link to='/' style={{ textDecoration:'none', color:'black' }}>  <Typography  sx={{'&:hover':{color:'brown'},borderBottom: '1px dashed gray',borderTop: '1px dashed gray', fontSize:20}}>Who We are</Typography></Link> 
+        <Link to='/' style={{ textDecoration:'none', color:'black' }}>  <Typography  sx={{'&:hover':{color:'#FF890E'},borderBottom: '1px dashed gray',borderTop: '1px dashed gray', fontSize:20}}>Who We are</Typography></Link> 
               <Box pb={1} sx={{ borderBottom: '1px dashed gray',}}>  <ButtonBase 
               component={Link} 
               to="/target-path" 
-               sx={{'&:hover':{backgroundColor:'brown'}, backgroundColor:'#ff6900', paddingX:6, height:50,width:150, color:'white', fontSize:20,borderRadius:1 }}>
+               sx={{'&:hover':{backgroundColor:'brown'}, backgroundColor:'#FF890E', paddingX:6, height:50,width:150, color:'white', fontSize:20,borderRadius:1 }}>
                 Register
             </ButtonBase>
             </Box>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,41 @@
 
-
-
+import { Box, IconButton, Stack, Typography } from '@mui/material'
 import React from 'react'
+import Navbar from '../components/navbar'
+import img1 from '../assets/img1.jpg'
+import SearchIcon from '@mui/icons-material/Search';
+
 
 function Home() {
   return (
-    <div style={{ height:1000 }}></div>
+    <Stack direction='row'
+      alignItems='center'
+      paddingLeft={7}
+       sx={{
+      height:790,
+      background: 'linear-gradient(to bottom, #FFFBF8, #FFCC97)', // Gradient colors
+    }}>
+      <Stack pr='5%' >
+        <Typography variant='h4' fontWeight='bolder' 
+         sx={{background:'linear-gradient(to right, #FF890E,#FFCC97)',
+          WebkitBackgroundClip: 'text',
+          color: 'transparent',
+         fontSize:140 , fontFamily:'sans-serif'}}>Order us</Typography>
+        <p style={{ fontSize:27 ,fontFamily:'sans-serif'}}>In publishing and graphic design, Lorem ipsum is a placeholder 
+          text commonly used to demonstrate the visual form of a document or a typeface without</p>
+        <Stack direction='row'  justifyContent='space-between' sx={{ backgroundColor:'white', paddingY:1, paddingX:1, paddingLeft:4, borderRadius:20 }}>
+          <input placeholder='Search' style={{ outline:'none',border:'none', fontSize:30 }} />
+          <Box p={1} sx={{ backgroundColor:'#FF890E', borderRadius:'100%' }}>
+            <IconButton>
+              <SearchIcon  sx={{fontSize:65 ,color:'white' }} />
+            </IconButton>
+          </Box>
+        </Stack>
+      </Stack>
+      <Box>
+        <img src={img1} style={{height:720}}/>
+      </Box>
+    </Stack>
   )
 }
 
